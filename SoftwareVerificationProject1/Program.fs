3eb58fc9ec25d70c65db6325004cdac6f2358819
Davide Albiero <davide2910@live.it>
2023-03-21 10:58:37 +0100

    Testato la libreria yacc

--- SoftwareVerificationProject1/Program.fs
+++ SoftwareVerificationProject1/Program.fs
@@ -1,3 +1,19 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open FSharp.Text.Lexing
 
+let evaluate (input:string) =
+    let lexbuf = LexBuffer<char>.FromString input
+    let output = Parser.parse Lexer.tokenize lexbuf
+    string output
+
+[<EntryPoint>]
+let main args =
+    printfn "Press Ctrl+c to Exit"
+    while true do
+        printf "Evaluate > "
+        let input = Console.ReadLine()
+        try
+            let result = evaluate input
+            printfn "%s" result
+        with ex -> printfn "%s" (ex.ToString())
+    0
