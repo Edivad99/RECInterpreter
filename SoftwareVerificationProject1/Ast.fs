73dd4280dfc94edb58d57badfc31ee16bd8251b3
Davide Albiero <davide2910@live.it>
2023-03-22 12:18:34 +0100

    ToString union type

--- SoftwareVerificationProject1/Ast.fs
+++ SoftwareVerificationProject1/Ast.fs
@@ -6,13 +6,16 @@ type Op =
     | Plus
     | Minus
     | Mult
+    override x.ToString() = sprintf "%A" x 
 
 type Expr =
-    | EVar of Variable
+    | EVar of Variable 
     | ENum of int option
     | ECond of Expr * Expr * Expr
     | EOp of Expr * Op * Expr
     | EFunc of Variable * Expr list
+    override x.ToString() = sprintf "%A" x 
+
 
 type FuncDec = Variable * Variable list * Expr
 
@@ -21,4 +24,3 @@ type Def = Variable * int option
 type VEnv = Def list
 
 type Program = FuncDec list * Expr * VEnv
-
