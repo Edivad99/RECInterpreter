a1d6333630ee59025c0fbb430b00dd1a1bf77b54
Davide Albiero <davide2910@live.it>
2023-07-18 20:58:20 +0200

    Fix test

--- RECInterpreter.Test/Tests.fs
+++ RECInterpreter.Test/Tests.fs
@@ -19,5 +19,5 @@ type TestInterpreter () =
     [<DataRow("f1(x) = 2 - x; f1(x); x = 2;", 0)>]
     [<DataRow("functional (n) = if n then 1 else functional(n - 1) * n; functional (x); x = 3;", 6)>]
     member this.TestProgram (program, expected_result: int) =
-        let result = interpreter(evaluate program)
+        let result = interpreter(evaluate program) 0
         Assert.AreEqual(expected_result, result);
